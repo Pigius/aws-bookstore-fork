a3c2c8f6860867a428d2efae23011bbf2f1ce8f9
Daniel Aniszkiewicz <daniel.aniszkiewicz@gmail.com>
2023-11-07 22:12:55 +0100

    Basic authorizer

---
+++ functions/APIs/authorizeStep.js
@@ -0,0 +1,26 @@
+"use strict";
+
+exports.handler = async (event, context, callback) => {
+  console.log("event", event);
+  console.log("event", context);
+  // Return immediately if being called by warmer
+  if (event.source === "warmer") {
+    return callback(null, "Lambda is warm");
+  }
+
+  const policyDocument = {
+    Version: "2012-10-17",
+    Statement: [
+      {
+        Action: "execute-api:Invoke",
+        Effect: "Allow",
+        Resource: event.methodArn,
+      },
+    ],
+  };
+
+  return {
+    principalId: "anonymous",
+    policyDocument,
+  };
+};
